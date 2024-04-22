f41ea48dfa63e80c1fc60fa85231b0df6ce3e1fe
LTPhat <102889533+LTPhat@users.noreply.github.com>
2024-04-23 01:14:30 +0700

    upload

---
+++ README.md
@@ -0,0 +1,3 @@
+<h2>VLSM Calculator</h2>
+<hr>
+<p>This is a VLSM (Variable Length Subnet Mask) calculator built with Reactjs</p>
---
+++ src/components/Subnet.js
@@ -0,0 +1,45 @@
+import React from "react";
+import AddressingModule from "../modules/AddressingModule";
+import "./Subnet.css";
+
+class Subnet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hosts: Number(this.props.hosts),
+            address: String(this.props.info.address),
+            cidr: Number(this.props.info.cidr),
+            subnet_mask: String(this.props.info.subnetMask)
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            default_gateway: AddressingModule.getDefaultGateway(this.state.address),
+            broadcast_address: AddressingModule.getBroadcastAddress(this.state.address, this.state.cidr),
+            last_host_address: AddressingModule.getLastHostAddress(AddressingModule.getBroadcastAddress(this.state.address, this.state.cidr)),
+            addresses_available: AddressingModule.getAvailableHostAddresses(this.state.cidr)
+        });
+    }
+
+    render() {
+        return (
+            <div className='ui raised segment' id='subnet_div'>
+                <div className='active title'>
+                        <span>
+                            <strong>Hosts: </strong>{this.state.hosts} | <strong>Address: </strong>{this.state.address}  | <strong>Subnet
+                            Mask: </strong>{this.state.subnet_mask} | <strong>CIDR: </strong>{this.state.cidr}</span>
+                    <i className='dropdown icon' id='dropdown_icon'/>
+                </div>
+                <div className='active content'>
+                    <p><strong>Default Gateway: </strong>{this.state.default_gateway} | <strong>Last Host
+                        Address: </strong> {this.state.last_host_address} | <strong>Broadcast
+                        Address: </strong>{this.state.broadcast_address}  </p>
+                    <p><strong>Available host addresses: </strong>{this.state.addresses_available}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Subnet;
